2ea74b6f5bbd9d17764e1e83c8f59e8e079b7b81
Miro <MiroPhoenix91@gmail.com>
2021-02-24 12:05:14 +0100

    vue 3 first project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Modal :header="header"></Modal>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title : 'My First Vue 3 Project',
+      header : "Sign up for the give away"
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom:10px ;
+}
+</style>
